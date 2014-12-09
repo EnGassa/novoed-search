2e31ba5a0b906a9b8713cc6449da2a9a6025aa47
Sahil <kumar.sahil@gmail.com>
2014-12-09 01:43:05 -0800

    Updated routes. Added models to users.js routes for access

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    models = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
